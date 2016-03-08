7e7077c53ba36cb3492cb0d4886a57bd8a2d379e
Jakob Hariz <jakob.hariz@gmail.com>
2016-03-08 13:05:46 +0100

    minor changes to visualize globule matlab script. Next up : need to verify that boundary conditions work, then add cylinder collisions

--- matlab/visualize_globule.m
+++ matlab/visualize_globule.m
@@ -4,13 +4,13 @@
 %data = load('../data/Visualize_Default.dna');
 data = load('Distance/debug/chain');
 N = size(data,1);
-plot_steps = 1;
-animate = false;
+plot_steps = 10;
+animate = true;
 
 lim = floor(N^(1/3));
 dt = 0.01;
 dn = floor(N/plot_steps);
-dn = 1;
+
 data(N,:);
 
 sp=15;
@@ -19,14 +19,28 @@ tic
 [Nc , reduced_globule] = cut_globule(data,spans);
 toc
 if animate
-for i = (dn+1):dn:N
-    
-    plot3(data((i-dn):i,1),data((i-dn):i,2),data((i-dn):i,3));
-    hold on;
-    plot3(data((i-dn):i,1),data((i-dn):i,2),data((i-dn):i,3),'.');
-    axis([-lim,lim, -lim,lim, -lim,lim]) 
-    pause(dt)
+    nrColors = 6;
+    c = prism(nrColors);
+    dp = floor(N/nrColors);
+%for i = (dn+1):dn:N
 
+ for i =[1:nrColors]
+
+        
+        sub_segment = data((i-1)*dp+1 : i*dp,:);
+        N2 = size(sub_segment,1);
+        dn2 = floor(N2/plot_steps);
+        
+       for j = (dn2+1):dn2:N2
+            hold on
+            plot3(sub_segment((j-dn2):j,1),sub_segment((j-dn2):j,2) ,sub_segment((j-dn2):j,3),...
+                '.-', 'color', c(i,:), 'LineWidth',3);
+            hold off
+            axis([-lim,lim, -lim,lim, -lim,lim]) 
+            
+            pause(dt) 
+            
+       end
 end
 else
     figure(1)
@@ -40,29 +54,21 @@ else
             'LineWidth',3);
         hold off
     end
+end
+xlabel('X')
+ylabel('Y')
+zlabel('Z')
     
-    xlabel('X')
-    ylabel('Y')
-    zlabel('Z')
     
-    
-    figure(2)
-    for k = 1:Nc
+figure(2)
+for k = 1:Nc
         
     p_gl = reduced_globule{k};
     N2 = size(p_gl,1);
     hold on
     plot3(p_gl(:,1),p_gl(:,2), p_gl(:,3),'.-','LineWidth',3);
     hold off
-    dp = floor(N2/nrColors);
-%     for i =[1:nrColors]
-%         hold on
-%         plot3(p_gl((i-1)*dp+1:i*dp,1),p_gl((i-1)*dp+1:i*dp,2),...
-%             p_gl((i-1)*dp+1:i*dp,3),'.-', 'color', c(i,:), ...
-%             'LineWidth',3);
-%         hold off
-%     end
-    end
+end
     
     
     
@@ -71,4 +77,4 @@ else
     zlabel('Z')
 
     %axis([-lim,lim, -lim,lim, -lim,lim])
-end
+
